976d1327e2b4d77d0c8908bdb8b7794875f384b3
Yoonjiwon-0305 <ygw8878@gmail.com>
2026-03-13 17:00:41 +0900

    0313 school

--- 0306_RV32I _R/0306_RV32I _R.srcs/sim_1/new/tb_RV32I.sv
+++ 0306_RV32I _R/0306_RV32I _R.srcs/sim_1/new/tb_RV32I.sv
@@ -19,8 +19,52 @@ module tb_RV32I ();
         @(negedge clk);
         reset = 0;
 
-        repeat (15) @(negedge clk);
+        repeat (800) @(negedge clk);
         $stop;
     end
 
 endmodule
+
+// ver1 
+/* Type your code here, or load an example. */
+//int adder(int a, int b);
+//void main(void) {
+//    int i = 0;
+//    int sum = 0;
+//    while(i<11) {
+//    sum = adder(i,sum);  
+//    i = i + 1;
+//}
+//    return ;
+//}
+//int adder(int a, int b){
+//    return a+b;
+//}
+///* Type your code here, or load an example. */
+
+
+// ver2
+//void swap(int *a, int *b);
+//void main(void) {
+//    int aNUM [6] = {3,1,4,0,2};
+//    int size = 5;
+//    int i = 0,j =0 ;
+//
+//    for (i=0; i < size-1; i++){
+//        for (j=0; j<(size-1); j++){
+//            if(aNUM[j] < aNUM[j+1]){
+//                swap(&aNUM[j],&aNUM[j+1]);
+//            }
+//        }
+//    }
+//
+//    while(1);
+//    return ;
+//}
+//void swap(int *a, int *b){
+//    int temp;
+//    temp = *b;
+//    *b = *a;
+//    *a = temp;
+//    return;
+//}
